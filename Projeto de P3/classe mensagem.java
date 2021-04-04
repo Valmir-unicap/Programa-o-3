164b90d6b51776a1750510b9536f7a9e42800793
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-04 20:45:36 -0300

    O código segue as seguintes exigências, abaixo:
    https://netuh.github.io/POO/17-03-2021-descicao-de-projeto/

---
+++ Projeto de P3/classe mensagem.java
@@ -0,0 +1,44 @@
+package Logica;
+/*
+Equipe:
+@author André
+@author Lucas
+@author Pedro
+@author Valmir
+ */
+public class Mensagem {
+    private String para;
+    private String de;
+    private String mensagem;
+
+    public Mensagem(String para, String de, String mensagem) {
+        super();
+        this.para = para;
+        this.de = de;
+        this.mensagem = mensagem;
+    }
+
+    public String getPara() {
+        return para;
+    }
+
+    public void setPara(String para) {
+        this.para = para;
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
