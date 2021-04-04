8dcf93165bb35114153d5e305c92bd2d0d62667b
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-04 20:39:33 -0300

    O código segue essas exigências, abaixo:
    https://netuh.github.io/POO/17-03-2021-descicao-de-projeto/

---
+++ Projeto de P3/classe amigos.java
@@ -0,0 +1,35 @@
+package Logica;
+/*
+Equipe:
+@author André
+@author Lucas
+@author Pedro
+@author Valmir
+*/
+public class Amigo {
+    private String para;
+    private String de;
+    private boolean pendente;
+
+    public Amigo(String de, String para) {
+        this.de = de;
+        this.para = para;
+        this.pendente = true;
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public String getPara() {
+        return para;
+    }
+
+    public void setPendente (boolean p) {
+        this.pendente = p;
+    }
+
+    public boolean getPendente () {
+        return this.pendente;
+    }
+}
