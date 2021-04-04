d779fec46684949a0ff218dffc60e7e285b0da62
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-04 20:27:52 -0300

    O código segue a seguintes exigências, abaixo:
    https://netuh.github.io/POO/17-03-2021-descicao-de-projeto/

---
+++ Projeto de P3/Aplicação.java
@@ -0,0 +1,8 @@
+package unikit;
+//import Logica.*;
+public class Aplicacao {
+    public static void main(String[] args) {
+        Unikut social = new Unikut();
+        social.menuStart();
+    }
+}
