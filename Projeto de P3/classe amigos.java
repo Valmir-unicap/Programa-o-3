81a73c0ab1e45bf4ba0bdaad78852307c2bcdaf9
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-06-02 04:13:33 -0300

    Update classe amigos.java

--- Projeto de P3/classe amigos.java
+++ Projeto de P3/classe amigos.java
@@ -1,35 +1,31 @@
-package Logica;
+package unikut.pkg2gq;
 /*
-Equipe:
-@author André
-@author Lucas
-@author Pedro
-@author Valmir
-*/
-public class Amigo {
-    private String para;
-    private String de;
-    private boolean pendente;
+ * @author valmir
+ */
+    public class Amigo {
+        private String para;
+        private String de;
+        private boolean pendente;
 
-    public Amigo(String de, String para) {
-        this.de = de;
-        this.para = para;
-        this.pendente = true;
-    }
+        public Amigo(String de, String para) {
+            this.de = de;
+            this.para = para;
+            this.pendente = true;
+        }
 
-    public String getDe() {
-        return de;
-    }
+        public String getDe() {
+            return de;
+        }
 
-    public String getPara() {
-        return para;
-    }
+        public String getPara() {
+            return para;
+        }
 
-    public void setPendente (boolean p) {
-        this.pendente = p;
-    }
+        public void setPendente (boolean p) {
+            this.pendente = p;
+        }
 
-    public boolean getPendente () {
-        return this.pendente;
+        public boolean getPendente () {
+            return this.pendente;
+        }
     }
-}
