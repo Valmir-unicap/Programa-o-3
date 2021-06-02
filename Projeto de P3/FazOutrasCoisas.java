cf9a232a273e3009cddd8a1c999bd90a039e061f
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-06-02 03:49:30 -0300

    Update FazOutrasCoisas.java

--- Projeto de P3/FazOutrasCoisas.java
+++ Projeto de P3/FazOutrasCoisas.java
@@ -1,5 +1,5 @@
 package unikit;
-/**
+/*
  * @author valmir
  */
 public class FazOutrasCoisas extends Thread{
@@ -7,7 +7,7 @@ public class FazOutrasCoisas extends Thread{
     @Override
     public void run(){
         int i;
-        for(i=100;i>0;i=i-1){
+        for(i=1;i>0;i=i-1){
             System.out.println("Thread continuando a execução");
         }
     }  
