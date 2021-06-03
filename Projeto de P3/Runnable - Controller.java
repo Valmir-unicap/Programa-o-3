c8a151caf61be864f33608e3343b483d26c085c5
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-06-03 02:21:58 -0300

    O código segue as seguintes exigências, abaixo:
    Todas as equipes devem:
    1. Incorporar as atividades anteriores em um branch só do repositório
    2. Reorganizar o código para que siga o MVC
    3. Usar Threats no código (Arquitetura concorrente)
    4. OPCIONAL - Fazer a Camada da VIEW deve ser executada de forma distribuída da camada do Controller e Model.

--- Projeto de P3/Runnable - Controller.java
+++ Projeto de P3/Runnable - Controller.java
@@ -1,5 +1,15 @@
 package java.lang;
 
+/*
+     Equipe:
+   @author André
+   @author Lucas
+   @author Pedro
+   @author Valmir
+*/
+
+//Observação: A Thread é acionada, quando o usuário adiciona um novo amigo.
+
 @FunctionalInterface
 public interface Runnable {
 
